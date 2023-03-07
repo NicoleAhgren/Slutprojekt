b372ae6e7d8012d5de94be655899de1f6488eeef
Nicole Åhgren <ahgren.nicole@elev.ga.ntig.se>
2023-03-07 10:16:11 +0100

    massa blä

--- Slutprojekt/Program.cs
+++ Slutprojekt/Program.cs
@@ -2,3 +2,28 @@
 
 // Skriv psuedokod å grejer
 
+Raylib.InitWindow(1000, 800, "IDunno"); // 1000 = bredden 800 = höjden
+Raylib.SetTargetFPS(60);
+
+string Level = "Start";
+while (Raylib.WindowShouldClose() == false)
+{
+        if (Level == "Start")
+    {
+    //     if (Raylib.IsKeyPressed(KeyboardKey.KEY_ENTER)) //När man trycker ENTER startar level 1
+    //     {
+    //         Level = "Spel";
+    //     }
+     }
+}
+
+
+Raylib.BeginDrawing();
+
+if (Level == "Start")
+{ 
+    Raylib.ClearBackground(Color.PURPLE);
+
+}
+
+Raylib.EndDrawing();
