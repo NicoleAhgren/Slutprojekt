83be6ba71c8a76c7391d3591ecfb33ded1e0ffb4
Nicole Åhgren <ahgren.nicole@elev.ga.ntig.se>
2023-03-21 10:28:40 +0100

    blö

--- Slutprojekt/Program.cs
+++ Slutprojekt/Program.cs
@@ -2,30 +2,43 @@
 
 // Skriv psuedokod å grejer
 
-Raylib.InitWindow(1000, 800, "IDunno"); // 1000 = bredden 800 = höjden
+Raylib.InitWindow(1200, 800, "IDunno"); // 1000 = bredden 800 = höjden
 Raylib.SetTargetFPS(60);
 
-//string Level = "Start";
+
+string Level = "Start";
+//Color LightGray = new Color(245, 245, 245, 0);
+
+Rectangle one = new Rectangle (0, 700, 1200, 100);
+
 
 while (Raylib.WindowShouldClose() == false)
 {
-    //     if (Level == "Start")
-    // {
-    //     if (Raylib.IsKeyPressed(KeyboardKey.KEY_ENTER)) //När man trycker ENTER startar level 1
-    //     {
-    //         Level = "Spel";
-    //     }
-    //  }
-}
+        if (Level == "Start")
+    {
+        if (Raylib.IsKeyPressed(KeyboardKey.KEY_ENTER)) //När man trycker ENTER startar level 1
+        {
+            Level = "Spel";
+        }
+     }
 
 
 Raylib.BeginDrawing();
 
-//if (Level == "Start")
-//{ 
-    Raylib.ClearBackground(Color.WHITE);
-    Raylib.DrawText("START", 400, 300, 100, Color.BLACK);
+if (Level == "Start")
+{ 
+    Raylib.ClearBackground(Color.LIGHTGRAY);
+    Raylib.DrawText("START", 425, 300, 100, Color.BLACK);
+    Raylib.DrawText("Press ENTER to play", 425, 650, 30, Color.BLACK);
 
-//}
+}
+
+if (Level == "Spel")
+{
+    Raylib.ClearBackground(Color.LIGHTGRAY);
+    Raylib.DrawRectangleRec(one, Color.BLACK);
+}
 
 Raylib.EndDrawing();
+
+}
