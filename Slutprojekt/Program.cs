718966955772ba93f8d8c3e1fa2d7e04ecad62ba
Nicole Åhgren <ahgren.nicole@elev.ga.ntig.se>
2023-03-28 09:42:36 +0200

    lost

--- Slutprojekt/Program.cs
+++ Slutprojekt/Program.cs
@@ -1,17 +1,21 @@
 ﻿using Raylib_cs;
+using System.Numerics;
 
 // Skriv psuedokod å grejer
 
-Raylib.InitWindow(1200, 800, "IDunno"); // 1000 = bredden 800 = höjden
+Raylib.InitWindow(1200, 800, "IDunno"); // 1200 = bredden 800 = höjden
 Raylib.SetTargetFPS(60);
 
-
 string Level = "Start";
-//Color LightGray = new Color(245, 245, 245, 0);
-//Color LightBlue = new Color(137, 207, 240, 0);
+// Color LightGray = new Color(245, 245, 245, 0);
+// Color LightBlue = new Color(137, 207, 240, 0);
 
-Rectangle Avatar = new Rectangle(0, 400, 40, 40);
+Texture2D Avatar = Raylib.LoadTexture("Avatar.png");
+float speed = 3f;
+
+Rectangle avatar = new Rectangle(0, 400, Avatar.width, Avatar.height); // (0, 400)
 Rectangle one = new Rectangle (0, 600, 1200, 200);
+
 
 
 while (Raylib.WindowShouldClose() == false)
@@ -24,6 +28,41 @@ while (Raylib.WindowShouldClose() == false)
         }
      }
 
+     if (Level == "Spel")
+     {
+        Vector2 AvatarMovement = new Vector2();
+
+        if (Raylib.IsKeyDown(KeyboardKey.KEY_UP) || Raylib.IsKeyDown(KeyboardKey.KEY_W))
+        {
+            AvatarMovement.Y = -speed;
+        }
+        if (Raylib.IsKeyDown(KeyboardKey.KEY_DOWN) || Raylib.IsKeyDown(KeyboardKey.KEY_S))
+        {
+            AvatarMovement.Y = +speed;
+        }
+        if (Raylib.IsKeyDown(KeyboardKey.KEY_RIGHT) || Raylib.IsKeyDown(KeyboardKey.KEY_D))
+        {
+            AvatarMovement.X = +speed;
+        }
+        if (Raylib.IsKeyDown(KeyboardKey.KEY_LEFT) || Raylib.IsKeyDown(KeyboardKey.KEY_A))
+        {
+            AvatarMovement.X = -speed;
+        }
+        
+        avatar.x += AvatarMovement.X;
+        avatar.y += AvatarMovement.Y;
+        //gör så att gubben inte kan gå utanför skärmen
+        if (avatar.x < 0 || avatar.x > Raylib.GetScreenWidth() - avatar.width)
+        {
+            avatar.x -= AvatarMovement.X;
+        }
+        if (avatar.y < 0 || avatar.y > Raylib.GetScreenHeight() - avatar.height)
+        {
+            avatar.y -= AvatarMovement.Y;
+        }
+
+        Vector2 AvatarPosition = new Vector2(avatar.x, avatar.y); 
+     }
 
 Raylib.BeginDrawing();
 
@@ -38,7 +77,7 @@ if (Level == "Start")
 if (Level == "Spel")
 {
     Raylib.ClearBackground(Color.LIGHTGRAY);
-    Raylib.DrawRectangleRec(Avatar, Color.BLACK);
+    Raylib.DrawTexture(Avatar, (int)avatar.x, (int)avatar.y, Color.WHITE);
     Raylib.DrawRectangleRec(one, Color.VIOLET);
 }
 
